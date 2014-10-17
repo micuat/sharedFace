6af73088d1d1aeba65fccd80aaa44031b1b30dd8
micuat <micuat@aim.com>
2014-10-17 14:27:51 +0900

    More shaders

--- kinectIrTrack/src/ofApp.cpp
+++ kinectIrTrack/src/ofApp.cpp
@@ -131,6 +131,7 @@ void ofApp::init() {
 	
 	jsMode = 0; // pen or stamp; really need this?
 	shaderMode = 0; // 0 for turn off, others for shader effects
+	drawPointCloud = false;
 	
 	// distortion shader
 #define STRINGIFY(A) #A
@@ -140,6 +141,55 @@ void ofApp::init() {
 	uniform vec2 ppoint;
 	uniform float elapsedTime;
 	uniform int shaderMode;
+	//generate a random value from four points
+	vec4 rand(vec2 A,vec2 B,vec2 C,vec2 D){ 
+		
+		vec2 s=vec2(12.9898,78.233); 
+		vec4 tmp=vec4(dot(A,s),dot(B,s),dot(C,s),dot(D,s)); 
+		
+		return fract(sin(tmp) * 43758.5453)* 2.0 - 1.0; 
+	} 
+	
+	//this is similar to a perlin noise function
+	float noise(vec2 coord,float d){
+		
+		vec2 C[4]; 
+		
+		float d1 = 1.0/d;
+		
+		C[0]=floor(coord*d)*d1; 
+		
+		C[1]=C[0]+vec2(d1,0.0); 
+		
+		C[2]=C[0]+vec2(d1,d1); 
+		
+		C[3]=C[0]+vec2(0.0,d1);
+		
+		
+		vec2 p=fract(coord*d); 
+		
+		vec2 q=1.0-p; 
+		
+		vec4 w=vec4(q.x*q.y,p.x*q.y,p.x*p.y,q.x*p.y); 
+		
+		return dot(vec4(rand(C[0],C[1],C[2],C[3])),w); 
+	} 
+	
+	vec3 rgb2hsv(vec3 c) {
+		vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+		vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+		vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+		
+		float d = q.x - min(q.w, q.y);
+		float e = 1.0e-10;
+		return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+	}
+	
+	vec3 hsv2rgb(vec3 c) {
+		vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+		vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+		return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+	}
 	void main(){
 		
 		gl_TexCoord[0] = gl_MultiTexCoord0;
@@ -158,7 +208,15 @@ void ofApp::init() {
 		if( shaderMode == 1 ) {
 			col = vec4(0.0, fract(pos.x/2.0+elapsedTime*3.141592*2.0), fract(pos.x/2.0+elapsedTime*3.141592*2.0), fract(pos.x/2.0+elapsedTime*3.141592*2.0));
 			col.a *= col.a;
-		}
+		} else if( shaderMode == 2 ) {
+			col = vec4(fract(pos.x/2.0+elapsedTime*3.141592*2.0), 0.0, 0.0, fract(pos.x/2.0+elapsedTime*3.141592*2.0));
+			col.a *= col.a;
+		} else if( shaderMode == 3 ) {
+			col = vec4(0.0, 1.0, 1.0, 1.0);
+			col.r += 0.10*noise(vec2(pos.x/1000.0 + elapsedTime/100.0, pos.y/10000.0 - elapsedTime/20.0), 200.0);
+			col.r += 0.05*noise(vec2(pos.x/100.0 - elapsedTime/100.0, pos.y/1000.0 - elapsedTime/50.0), 200.0);
+			col.rgb = hsv2rgb(col.rgb);
+		}			
 		gl_FrontColor = col;
 	}
 	);
@@ -659,15 +717,16 @@ void ofApp::updateReceiveOsc() {
 		else if(m.getAddress() == "/mode/change"){
 			int mode = m.getArgAsInt32(3);
 			if( mode <= 1 ) {
-				shaderMode = 0;
+				//shaderMode = 0;
 				jsMode = mode;
 			}
 			else if( mode == 7 ) drawPointCloud = !drawPointCloud;
-			else if( mode == 2 ) {
-				if( shaderMode == 1 ) {
+			else {
+				int shaderModeToBe = mode - 1;
+				if( shaderMode == shaderModeToBe ) { // toggle
 					shaderMode = 0;
 				} else {
-					shaderMode = 1;
+					shaderMode = shaderModeToBe; // new one
 				}
 			}
 			
@@ -686,7 +745,12 @@ void ofApp::draw() {
 		drawImage.begin();
 		
 		ofScale(RES_MULT, RES_MULT); // scale for hyper resolution
-		ofBackground(0, 255);
+		if( shaderMode == 0 ) {
+			ofBackground(0, 255);
+		} else {
+			ofBackground(255, 255);
+		}
+
 		ofPushStyle();
 		
 		ofSetColor(255, 255);
@@ -703,7 +767,7 @@ void ofApp::draw() {
 		
 		// lines
 		ofSetLineWidth(2 * RES_MULT);
-		for( int i = 0; i < fatLines.size(); i++ ) {
+		for( int i = fatLines.size() - 1; i >= 0; i-- ) {
 			fatLines.at(i).draw();
 		}
 		
@@ -778,13 +842,9 @@ void ofApp::draw() {
 		shader.setUniform1f("elapsedTime", ofGetElapsedTimef());
 		shader.setUniform1i("shaderMode", shaderMode);
 		
-		if( shaderMode == 0 ) {
-			drawImage.getTextureReference().bind();
-		}
+		drawImage.getTextureReference().bind();
 		initMesh.draw();
-		if( shaderMode == 0 ) {
-			drawImage.getTextureReference().unbind();
-		}
+		drawImage.getTextureReference().unbind();
 		
 		if( cameraMode == EASYCAM_MODE ) {
 			cam.end();
